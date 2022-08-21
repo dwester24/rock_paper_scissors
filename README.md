05604b8db90aefb72069247fe7731f1eaa6d0f9b
Daniel Wester <dwester24@gmail.com>
2022-08-20 23:22:56 -0400

    Added game functionality
    Added UI in HTML and finished game functionality in JS.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # rock_paper_scissors
 The Odin Project Rock Paper Scissors Game
+
+Photo Credits:
+
+Rock: Photo by <a href="https://unsplash.com/@davehoefler?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Dave Hoefler</a> on <a href="https://unsplash.com/s/photos/rock?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Paper:
+
+Scissors:
--- game.js
+++ game.js
@@ -1,6 +1,17 @@
 // Global Variables
 let playerScore = 0;
 let computerScore = 0;
+let roundCount = 0;
+// Buttons
+const rock = document.getElementById('rockBtn');
+const paper = document.getElementById('paperBtn');
+const scissors = document.getElementById('scissorsBtn');
+const reset = document.getElementById('reset');
+// HTML elements to update
+const playerSc = document.getElementById('playerScore');
+const computerSC = document.getElementById('computerScore');
+const results = document.getElementById('results');
+
 
 // Randomly get computer's choice
 const getComputerChoice = () => {
@@ -10,50 +21,71 @@ const getComputerChoice = () => {
 }
 
 // Plays Single round of game
-const playRound = (playerSelection, computerSelection) => {
-    let player = playerSelection.toLowerCase();
-    let computer = computerSelection.toLowerCase();
+const playRound = (playerSelection) => {
+    if (roundCount < 5) {
+        roundCount += 1;
+        let player = playerSelection.toLowerCase();
+        let computer = getComputerChoice().toLowerCase();
 
-    return checkRoundWinner(player, computer);
-
-}
-
-// Play the game for 5 rounds
-const game = () => {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = window.prompt('Enter your selection:');
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+        return checkRoundWinner(player, computer);
+    } else {
+        return checkGameWinner()
     }
 
-    return checkGameWinner();
-
 }
-
 
 // Helper function to check round winner
 const checkRoundWinner = (player, computer) => {
     if (player === computer) {
-        return 'You tied!';
+        results.innerHTML = 'You tied! Try again!';
     } else if ((player === 'rock' && computer === 'scissors') ||
                (player === 'paper' && computer === 'rock') ||
                (player === 'scissors' && computer === 'paper')){
 
                 playerScore ++;
-                return `You Win! ${player} beats ${computer}`;
+                playerSc.innerHTML = `Player: ${playerScore}`;
+                results.innerHTML = `Round result: You Win! ${player} beats ${computer}`;
     } else {
         computerScore ++;
-        return `You Lose! ${computer} beats ${player}`;
+        computerSC.innerHTML = `Computer: ${computerScore}`;
+        results.innerHTML = `Round result: You Lose! ${computer} beats ${player}`;
     }
 }
 
 // Helper function to check game winner
 const checkGameWinner = () => {
     if (playerScore > computerScore) {
-        return `You Win!`
+        results.innerHTML = `You Win!`;
+    } else if (playerScore < computerScore) {
+        results.innerHTML = 'You Lose!';
     } else {
-        return 'You Lose!'
+        results.innerHTML = 'You tied!';
     }
 };
 
-console.log(game());
+// Add Event listeners
+rock.addEventListener('click', function() {
+    playRound('rock');
+
+});
+
+paper.addEventListener('click', function() {
+    playRound('paper');
+
+});
+
+scissors.addEventListener('click', function() {
+    playRound('scissors');
+})
+
+reset.addEventListener('click', function() {
+    // Reset UI
+    results.innerHTML = 'Round result: ';
+    playerSc.innerHTML = 'Player: 0';
+    computerSC.innerHTML = 'Computer: 0';
+
+    // Reset game variables
+    roundCount = 0;
+    playerScore = 0;
+    computerScore = 0;
+});
